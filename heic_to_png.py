c8a372479bed35f7d9cf030a316f59cb5a77925a
Stezzly <43850376+Stezzly@users.noreply.github.com>
2025-01-16 22:30:33 -0330

    feat: Better-defined output folder
    - User can choose the output path they would like to export to.
    - If output path folder does not exist, then automatically creates it in the root 'heic_to_png_converter' folder.

--- heic_to_png.py
+++ heic_to_png.py
@@ -2,8 +2,8 @@ import os
 import pillow_heif
 from PIL import Image
 
-def convert_heic_to_png(input_folder):
-    # List all files in the directory
+def convert_heic_to_png(input_folder, output_folder):
+    # List all files in the input directory
     for filename in os.listdir(input_folder):
         if filename.lower().endswith(".heic"):
             heic_file_path = os.path.join(input_folder, filename)
@@ -12,11 +12,13 @@ def convert_heic_to_png(input_folder):
             heif_file = pillow_heif.open_heif(heic_file_path)
             image = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data)
             
-            # Save the file as PNG
-            png_file_path = os.path.join(input_folder, f"{os.path.splitext(filename)[0]}.png")
+            # Save the file as PNG in the output folder
+            png_file_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}.png")
             image.save(png_file_path, "PNG")
             print(f"Converted {filename} to {os.path.basename(png_file_path)}")
 
 if __name__ == "__main__":
     input_folder = "heic_input_folder"  # Change this to the directory where your HEIC files are located
-    convert_heic_to_png(input_folder)
+    output_folder = "png_output_folder"  # Change this to the desired output directory where the PNG files will be saved
+    os.makedirs(output_folder, exist_ok=True)  # Create the output folder if it doesn't exist
+    convert_heic_to_png(input_folder, output_folder)
